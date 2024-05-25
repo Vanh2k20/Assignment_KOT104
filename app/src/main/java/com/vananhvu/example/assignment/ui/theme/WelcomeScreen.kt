f7f5e3f42a3177c8ef335cb2b9db0d14c7216f96
Vanh2k20 <avan81837@gmail.com>
2024-05-25 11:34:22 +0700

    Initial commit

---
+++ app/src/main/java/com/vananhvu/example/assignment/ui/theme/WelcomeScreen.kt
@@ -0,0 +1,123 @@
+package com.vananhvu.example.assignment.ui.theme
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.vananhvu.example.assignment.R
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun WelcomeScreen(navController: NavHostController) {
+	Scaffold(
+	
+	) {
+		Box(modifier = Modifier.fillMaxSize()) {
+			Image(
+				painter = painterResource(id = R.drawable.welcome), contentDescription = "",
+				modifier = Modifier.fillMaxSize(),
+				contentScale = ContentScale.Crop
+			)
+			Column(
+				modifier = Modifier
+					.fillMaxSize()
+					.padding(16.dp)
+					.fillMaxWidth(),
+				verticalArrangement = Arrangement.SpaceEvenly,
+				horizontalAlignment = Alignment.Start,
+			) {
+				Column(
+					modifier = Modifier.fillMaxWidth(),
+					horizontalAlignment = Alignment.Start
+				) {
+					Text(
+						text = "MAKE YOUR",
+						fontSize = 30.sp,
+						fontFamily = FontFamily(Font(R.font.gelasio_variablefont_wght)),
+						fontWeight = FontWeight.W600,
+						color = Color(0xFF606060)
+					)
+					Text(
+						text = "HOME BEAUTIFUL",
+						fontSize = 35.sp,
+						fontFamily = FontFamily(Font(R.font.gelasio_variablefont_wght)),
+						fontWeight = FontWeight.W700,
+						color = Color.Black
+					)
+					Spacer(modifier = Modifier.height(100.dp))
+					Box(
+						modifier = Modifier.fillMaxWidth()
+							.padding(top = 15.dp),
+						contentAlignment = Alignment.Center,
+					) {
+						Text(
+							text = "The best simple place where you discover most wonderful furnitures and make your home beautiful",
+							textAlign = TextAlign.Justify,
+							modifier = Modifier.width(300.dp),
+							fontSize = 18.sp,
+							style = TextStyle(
+								lineHeight = 30.sp
+							),
+							fontFamily = FontFamily(Font(R.font.nunitosans)),
+							fontWeight = FontWeight.W700,
+							color = Color(0xFF808080)
+						)
+					}
+				}
+				Box(
+					modifier = Modifier
+						.clip(RoundedCornerShape(8.dp))
+						.width(160.dp)
+						.height(50.dp)
+						.background(color = Color.Black)
+						.clickable { navController.navigate("login") }
+						.fillMaxWidth()
+						.align(Alignment.CenterHorizontally),
+					contentAlignment = Alignment.Center
+				
+				) {
+					Text("Get Started", fontSize = 20.sp, color = Color.White)
+				}
+			}
+			
+		}
+	}
+	
+	
+}
+
+@Preview(showBackground = true, showSystemUi = true, device = "id:pixel_6_pro")
+@Composable
+fun WelcomeScreenPreview() {
+	val navController = rememberNavController()
+	WelcomeScreen(navController = navController)
+}
